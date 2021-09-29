5fb2beb126339b4aa6cdafe711e44a4f1c9e35d5
Jerry Gregoire <jerry@arcadecoffee.com>
2021-09-29 17:08:00 -0500

    Init

--- stop_airflow.sh
+++ stop_airflow.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-kill $(cat .airflow/airflow-scheduler.pid)
-kill $(cat .airflow/airflow-webserver-monitor.pid)
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+SCHEDULER_PID_FILE=${SCRIPT_DIR}/.airflow/airflow-scheduler.pid
+WEBSERVER_PID_FILE=${SCRIPT_DIR}/.airflow/airflow-webserver-monitor.pid
+
+declare -a file_names=("$SCHEDULER_PID_FILE" "$WEBSERVER_PID_FILE")
+
+for file in "${file_names[@]}"; do
+  if [[ -f "$file" ]]; then
+      kill "$(cat $file)"
+  fi
+done
